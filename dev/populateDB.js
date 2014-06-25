d31b67a9280728b30f910a74b9fb4c726605f1a4
Florian Quattrocchi <florianquattrocchi@gmail.com>
2014-06-25 23:54:49 +0200

    SC player OK + Starting YT

--- dev/populateDB.js
+++ dev/populateDB.js
@@ -30,19 +30,70 @@ var PlaylistObj = {
   modificationDate: new Date()
 }
 
-for(var i = 0; i < 20; i++) {
-  var AssetObj = {
-    title: 'Title n° ' + (i + 1),
-    author: 'somebody',
-    addedBy: 'somebody',
-    addedDate: new Date(),
-    url: 'www.dzfzeg.fef',
-    src: 'dzzdfaev',
-    order: i,
-    service: 'Soundcloud', 
-    duration: '3:' + (10+i)
-  }
-
+var toAdd = [
+    {
+      "title" : "Ginyu Force (Iglooghost Rmx) - [The ¡itisi! EP Is Out Now! Link In Description.]",
+      "artist" : "siryote",
+      "url" : "http://soundcloud.com/villagemeister/ginyu-force-iglooghost-rmx",
+      "src" : "153302324",
+      "service" : "Soundcloud",
+      "duration" : "1:32",
+      "addedDate" : "Wed Jun 25 2014 21:14:09 GMT+0200 (CEST)",
+      "order" : 0
+    },
+    {
+      "title" : "Witty - Leave ft. Trippz Michaud (Prod. By Redhooknoodles)",
+      "artist" : "WittyOfficial",
+      "url" : "http://soundcloud.com/wittyofficial/witty-leave-ft-trippz-michaud-prod-by-redhooknoodles",
+      "src" : "155728193",
+      "service" : "Soundcloud",
+      "duration" : "3:40",
+      "addedDate" : "Tue Jun 24 2014 07:59:09 GMT+0200 (CEST)",
+      "order" : 1
+    },
+    {
+      "title" : "Takers feat. Chipper - Alive (Matinée - Amnesia Ibiza 2012)  - Official Video HD",
+      "artist" : "VuzMI",
+      "url" : "http://www.youtube.com/watch?v=Wy__crtdd9I",
+      "src" : "Wy__crtdd9I",
+      "service" : "Youtube",
+      "duration" : "3:19",
+      "addedDate" : "Mon Jun 23 2014 22:34:10 GMT+0200 (CEST)",
+      "order" : 2
+    },
+    {
+      "title" : "Coldplay - Viva La Vida",
+      "artist" : "parlophone",
+      "url" : "http://www.youtube.com/watch?v=dvgZkm1xWPE",
+      "src" : "dvgZkm1xWPE",
+      "service" : "Youtube",
+      "duration" : "4:03",
+      "addedDate" : "Tue Jun 24 2014 08:01:58 GMT+0200 (CEST)",
+      "order" : 3
+    },
+    {
+      "title" : "Tinie Tempah - Written In The Stars ft. Eric Turner",
+      "artist" : "parlophone",
+      "url" : "http://www.youtube.com/watch?v=YgFyi74DVjc",
+      "src" : "YgFyi74DVjc",
+      "service" : "Youtube",
+      "duration" : "3:37",
+      "addedDate" : "Tue Jun 24 2014 08:00:17 GMT+0200 (CEST)",
+      "order" : 4
+    },
+    {
+      "title" : "Deorro - Live @ EDC Las Vegas 2014",
+      "artist" : "EDC Las Vegas 2014*",
+      "url" : "http://soundcloud.com/edmchicagorecords/deorro-live-edc-las-vegas-2014",
+      "src" : "155596469",
+      "service" : "Soundcloud",
+      "duration" : "54:35",
+      "addedDate" : "Tue Jun 24 2014 07:59:44 GMT+0200 (CEST)",
+      "order" : 5
+    }
+];
+for(var track in toAdd) {
+  var AssetObj = toAdd[track];
   PlaylistObj.content = PlaylistObj.content.concat(new Array(AssetObj));
 }
 
@@ -53,3 +104,8 @@ newPlaylist.save(function(err) {
   });
 
 console.log(PlaylistObj);
+
+
+
+
+
--- public/js/controllers/player.js
+++ public/js/controllers/player.js
@@ -22,6 +22,9 @@ DiskoApp.controller('playerController', function($scope, $sce) {
       $scope.playlist.owner = data.content[0].owner;
       $scope.playlist.name = data.content[0].id;
       $scope.playlist.content = data.content[0].content;
+      //initialize the player
+      //$scope.playing.changeTrack(data.content[0].content[0]);
+      $scope.playing.init(data.content[0].content[0]);
     });
   });
 
@@ -103,193 +106,95 @@ DiskoApp.controller('playerController', function($scope, $sce) {
   };
   //manages the player
   $scope.playing = {};
-  $scope.playing.soundcloud = 'https://w.soundcloud.com/player/?auto_play=false&amp;hide_related=false&amp;visual=true&amp;url=https%3A//api.soundcloud.com/tracks/';
+  $scope.playing.soundcloud = 'http://api.soundcloud.com/tracks/';
   $scope.playing.youtube = 'http://www.youtube.com/embed/'; 
-  $scope.playing.current = $sce.trustAsResourceUrl($scope.playing.youtube + '5RdjiJP4yg4');
-  $scope.playing.changeTrack = function(track) {
-    console.log(track);
-    $scope.playing.current = $sce.trustAsResourceUrl($scope.playing[track.service.toLowerCase()] + track.src);
+  $scope.playing.currentURL = null;
+  $scope.playing.currentTrack = null;
+  $scope.playing.scWidget = null;
+  $scope.playing.isSC = false;
+
+  $scope.playing.init = function (track) {
+    $scope.playing.currentTrack = track;
+
+    if(track.service == 'Soundcloud'){
+      $scope.playing.isSC = true;
+      $('#scPlayer').attr('src', 'https://w.soundcloud.com/player/?visual=true&url=http://api.soundcloud.com/tracks/' + track.src);
+    }
+
+    $('#scPlayer').load(function(){ //binds events when the iframe is loaded
+      console.log("LOADED");
+
+      var iframeElement   = document.querySelector('#scPlayer');
+      $scope.playing.scWidget = SC.Widget(iframeElement);
+
+      $scope.playing.scWidget.bind(SC.Widget.Events.FINISH, function(){ //ADDS ACTION for terminating song
+        if($scope.playlist.content.length > parseInt($scope.playing.currentTrack.order)+1 && $scope.playing.isSC){ //stops if no more track
+          $scope.$apply(function () {
+            $scope.playing.changeTrack($scope.playlist.content[parseInt($scope.playing.currentTrack.order) + 1]);
+          });
+          console.log('TERMINATED');
+        }
+        else
+          console.log('STOP');
+      });
+    });
   };
+
+  $scope.playing.changeTrack = function(track) {
+    console.log('NEXT TRACK');
+    console.log(track);
+    $scope.playing.currentTrack = track;
+
+    if(track.service == 'Soundcloud') {
+      $scope.playing.isSC = true;
+      
+      console.log('CASE sc');
+      $scope.playing.scWidget.load($scope.playing.soundcloud + track.src, {
+        visual: true
+      });
+    }
+    if(track.service == 'Youtube') {
+      $scope.playing.isSC = false;
+      console.log('CASE yt');
+
+      player = new YT.Player('player', {
+        width: 1280,
+        height: 720,
+        videoId: track.src,
+        events: {
+          'onReady': function (event) {
+            event.target.playVideo();
+          }
+        }
+      });
+
+    }
+    //$scope.playing.currentURL = $sce.trustAsResourceUrl($scope.playing[track.service.toLowerCase()] + track.src);
+  };
+
+  //buttons to control the tracks
+  //docs soundcloud => http://developers.soundcloud.com/docs/api/html5-widget#events
+  $scope.buttons = {};
+  $scope.buttons.play = function () {
+    if($scope.playing.currentTrack.service == 'Soundcloud')
+      $scope.playing.scWidget.play();
+  };
+
 });
-//http://jqueryui.com/sortable/
-//http://stackoverflow.com/questions/5957916/how-to-handle-youtube-video-events-started-finished-etc-in-uiwebview-ios
-//from shufflecloud 
-//DONT FORGET script(src="http://connect.soundcloud.com/sdk.js")
-// var tracks = null;
-// var id = 0;
-// var precedent_action = 'next';
-// var playing = null;
+    // function onYouTubeIframeAPIReady() {
+    //     console.log('here');
+    //     var player;
+    //     player = new YT.Player('player', {
+    //       width: 1280,
+    //       height: 720,
+    //       videoId: 'M7lc1UVf-VE',
+    //       events: {
+    //         'onReady': onPlayerReady
+    //       }
+    //     });
+    //   }
 
-// function getXMLHttpRequest() {
-//   var xhr = null;
-//   if (window.XMLHttpRequest || window.ActiveXObject) {
-//     if (window.ActiveXObject) {
-//       try {
-//         xhr = new ActiveXObject("Msxml2.XMLHTTP");
-//       } catch(e) {
-//         xhr = new ActiveXObject("Microsoft.XMLHTTP");
-//       }
-//     } else {
-//       xhr = new XMLHttpRequest(); 
-//     }
-//   } else {
-//     alert("Pas d'Ajax, dommage!");
-//     return null;
-//   }
-//   return xhr;
-// }
-
-// //asynchronous call to the update process
-// function shuffle(exist_url) {
-//   stop_all();
-
-//   var req = getXMLHttpRequest();
-//   //var url ='https://soundcloud.com/florian-quattrocchi/sets/chiiiilllll';
-//   if(exist_url == null)
-//     var url = document.getElementById("url").value;
-//   else 
-//     var url = exist_url;
-
-//   req.onreadystatechange = function() {
-//     if (req.readyState == 4 && (req.status == 200 )) {
-      
-//       if(req.responseText == "This is not a playlist" || req.responseText == "Add an URL") {
-
-//         document.getElementById("div_url").className = "form-group has-feedback has-error";  
-//         document.getElementById("span_glyph_url").className = "glyphicon glyphicon-remove form-control-feedback";
-
-//         document.getElementById("button_error").className = "btn btn-danger pull-right";
-//         document.getElementById("button_error").innerHTML = req.responseText;
-//       } else {
-//         if(( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false )) {
-//           document.getElementById("iphone_play").className = "row";    
-//         }
-//         document.getElementById("div_url").className = "form-group has-feedback";  
-//         document.getElementById("span_glyph_url").className = "hidden";
-
-//         document.getElementById("button_error").className = "btn btn-success pull-right";
-//         document.getElementById("button_error").innerHTML = "Playlist shuffled"
-        
-//         tracks = JSON.parse(req.responseText);
-//         list_played();
-//         next();
-//       }
-//     }
-//   };
-//   req.open("GET", "add.php?url=" + url , true);
-//   req.send(null);
-// }
-
-// function playTrack() { 
-//   display_control();
-
-//   SC.stream("/tracks/" + tracks[id]['sc_id'], {
-
-//     onfinish: function(){ 
-//       next();
-//     }
-//   }, function(sound){
-    
-//     playing = sound.id;
-//     sound.start();
-//   });   
-// }
-
-// function next(){
-//   play();
-//   soundManager.stopAll();
-
-//   if(precedent_action == 'previous')
-//     id++;
-//   playTrack();
-//   precedent_action = 'next';
-//   if(id != tracks.length)
-//     id++;
-//   else 
-//     stop_all();
-// }
-// function previous(){
-//   play();
-//   soundManager.stopAll();
-
-//   if(id>0)
-//     id--;
-//   playTrack();
-//   precedent_action = 'previous';
-// }
-
-// function play(){
-//   soundManager.resume(playing);
-
-//   document.getElementById("img_play").className = "hidden";
-//   document.getElementById("img_pause").className = "";
-// }
-// function pause(){
-//   soundManager.pause(playing);
-
-//   document.getElementById("img_play").className = "";
-//   document.getElementById("img_pause").className = "hidden";
-// }
-// function stop_all() {
-//   soundManager.stopAll();
-//   id = 0;
-//   hide_control();
-// }
-
-// function display_control(){
-//   document.getElementById("div_control_panel").className = "row";
-//   document.getElementById("img_artwork").src = tracks[id]['picture'];
-//   document.getElementById("track_name").innerHTML = tracks[id]['title'] + ' - <i>by</i> ' + tracks[id]['artist'];
-// }
-
-// function hide_control(){
-//   document.getElementById("div_control_panel").className = "hidden";
-// }
-
-// function hide_play_iphone() {
-//   id--;
-//   next();
-//   document.getElementById("iphone_play").className = "hidden";  
-// }
-
-
-
-// function list_played() {
-
-//   var req = getXMLHttpRequest();
-
-//   req.onreadystatechange = function() {
-//     if (req.readyState == 4 && (req.status == 200 )) {
-//       var result = JSON.parse(req.responseText);
-
-//       if(document.getElementById("list_top"))
-//         document.getElementById("list_top").remove();
-//       if(document.getElementById("list_low"))
-//         document.getElementById("list_low").remove();
-
-//       var listTop = document.createElement("ol");
-//       listTop.id = "list_top";
-//       var listLow = document.createElement("ol");
-//       listLow.id = "list_low";
-      
-//       for( var i =  0 ; i < Math.min(result.length, 5) ; i++){
-//         var listItem = document.createElement("li");
-//         listItem.innerHTML = '<a href="javascript:shuffle(\'' + result[i][0] + '\')">' + result[i][1] + '</a>';
-//         listTop.appendChild(listItem);
-//       }
-//       for(var i =  result.length-1 ; i >= Math.max(result.length-5, 0) ; i--){
-//         var listItem = document.createElement("li");
-//         listItem.innerHTML = '<a href="javascript:shuffle(\'' + result[i][0] + '\')">' + result[i][1] + '</a>';
-//         listLow.appendChild(listItem);
-//       }
-
-//       document.getElementById("col_top").appendChild(listTop);
-//       document.getElementById("col_low").appendChild(listLow);
-
-//     }
-//   };
-//   req.open("GET", "get_lists.php", true);
-//   req.send(null);
-
-
-// }
+      function onPlayerReady(event) {
+        event.target.setVolume(100);
+        event.target.playVideo();
+      }
